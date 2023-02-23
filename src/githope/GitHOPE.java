d83719c0a3ade0c881d88412d7398665fcb1fc26
AofTHz <AofTHz@Aof-PC>
2023-02-23 09:55:10 +0700

    BRUH SECOND ONE

--- src/githope/GitHOPE.java
+++ src/githope/GitHOPE.java
@@ -16,6 +16,7 @@ public class GitHOPE {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("BSSSS");
+        System.out.println("FUCK YOU");
     }
     
 }
